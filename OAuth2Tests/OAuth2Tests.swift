a6ac7f60d89df200f9c1e59695e38dd2ff605c25
vkandel <vijaya.prakash.kandel@zalando.de>
2018-01-08 22:35:14 +0100

    Initial Commit

---
+++ OAuth2Tests/OAuth2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  OAuth2Tests.swift
+//  OAuth2Tests
+//
+//  Created by Vijaya Prakash Kandel on 08.01.18.
+//  Copyright © 2018 com.kandelvijaya. All rights reserved.
+//
+
+import XCTest
+@testable import OAuth2
+
+class OAuth2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
