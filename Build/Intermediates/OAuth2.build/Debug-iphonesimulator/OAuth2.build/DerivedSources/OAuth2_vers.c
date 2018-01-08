3411f6a3d46bfddeb2cd7a0af7504b31dea08c3e
kandelvijaya <kandelvijaya@gmail.com>
2018-01-08 22:44:49 +0100

    gitignored build directory

--- Build/Intermediates/OAuth2.build/Debug-iphonesimulator/OAuth2.build/DerivedSources/OAuth2_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char OAuth2VersionString[];
- extern const double OAuth2VersionNumber;
-
- const unsigned char OAuth2VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OAuth2  PROJECT:OAuth2-1" "\n";
- const double OAuth2VersionNumber __attribute__ ((used)) = (double)1.;
