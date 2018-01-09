5ff751736fba51ec934e1b659d6d3778601e7938
kandelvijaya <kandelvijaya@gmail.com>
2018-01-09 23:41:01 +0100

    Added needed resources

---
+++ OAuthorize2.podspec
@@ -0,0 +1,36 @@
+# coding: utf-8
+#
+
+Pod::Spec.new do |s|
+
+  s.name         = "OAuth2"
+  s.version      = "0.1.0"
+  s.summary      = "OAuth(orize)2 facilitates API interaction with simple Config"
+
+  s.description  = <<-DESC
+                   This library is handy if one wants to do OAuth2 based REST API usage. 
+                   The only requirement for the library to enable OAuth2 is the config
+                   file composed of
+                   - tokenServerURL
+                   - authServerURL
+                   - scopes
+                   - client_id
+
+                   DESC
+
+  s.homepage     = "https://github.com/kandelvijaya/OAuth2.git"
+  s.license      = "MIT"
+
+  s.author             = { "kandelvijaya" => "kandelvijaya@gmail.com" }
+  s.social_media_url   = "http://twitter.com/kandelvijaya"
+
+  s.platform     = :ios, "9.0"
+  s.source       = { :git => "https://github.com/kandelvijaya/OAuth2.git", :tag => "#{s.version}" }
+
+  s.source_files  = 'OAuth2/**/*.{swift,h,m}'
+  s.pod_target_xcconfig = { 'SWIFT_VERSION' => '4' }
+
+  s.dependency "Kekka", "~> 0.6"
+  s.dependency "CommonCryptoModule"
+
+end
---
+++ OAuthorize2Tests/OAuth2Tests.swift
@@ -0,0 +1,84 @@
+//
+//  Copyright © 2018 zalando.de. All rights reserved.
+//
+
+import XCTest
+@testable import OAuth2
+import Kekka
+
+final class OAuth2Tests: XCTestCase {
+
+    private let token = OAuth2AccessToken(accessToken: "token", tokenType: "kind", expiresIn: 100, refreshToken: "refreshToken")
+    let service: OAuth2 = OAuthMock()
+    lazy var network = (service.accessTokenNetworkService as? MockNetowrk)
+
+    func test_whenAskForAccessTokenWithAuthCodeIsCalled_thenAPIIscalled() {
+        let authCode = "123"
+        network?.eventual(token: token)
+        print(service.accessTokenNetworkService)
+        service.askForAccessToken(with: authCode).then { tk in
+            if case let .success(tk1) = tk {
+                XCTAssertEqual(tk1.accessToken, self.token.accessToken)
+            } else {
+                XCTFail("Token doesnot match")
+            }
+        }.execute()
+    }
+
+    func test_whenAskforAccessTokenWithAuthCodeIsCalled_thenReturnedTokenIsStored() {
+        let authCode = "123"
+        network?.eventual(token: token)
+        service.askForAccessToken(with: authCode).execute()
+        let retrieved = service.accessTokenStorageService.retrieve(tokenFor: service.config)
+        XCTAssertNotNil(retrieved)
+        XCTAssertEqual(retrieved!.accessToken, token.accessToken)
+    }
+
+    func test_whenTokenForGivenURLIsNotInStorage_thenIsAuthroizationRequiredReturnsTrue() {
+        service.accessTokenStorageService.delete(tokenFor: service.config)
+        XCTAssertTrue(service.isAuthorizationRequired())
+    }
+
+}
+
+
+final class OAuthMock: OAuth2 {
+
+    var config: OAuth2Config {
+        let clientId = "clientId"
+        let scopes = ["username", "password"]
+        let authServer = URL(string: "www.kandelvijaya.com")!
+        let tokenServer = URL(string: "www.bnk.life")!
+        let redirectURI = URL(string: "myapp://")!
+        let config = OAuth2Config(clientId: clientId, scopes: scopes, authServer: authServer, tokenServer: tokenServer, redirectURI: redirectURI)
+        return config
+    }
+
+    // Check this out: https://twitter.com/kandelvijaya/status/950055427882016768
+    var accessTokenNetworkService: OAuth2NetworkServiceProtocol = MockNetowrk()
+}
+
+
+final class MockNetowrk: OAuth2NetworkServiceProtocol {
+
+    enum MockError: Error {
+        case testMockError
+    }
+
+    private var token: Result<OAuth2AccessToken> = .failure(error: MockError.testMockError)
+
+    func eventual(token: OAuth2AccessToken) {
+        self.token = .success(value: token)
+    }
+
+    func eventual(error: Error) {
+        self.token = .failure(error: error)
+    }
+
+    func post(withRequest urlRequest: URLRequest) -> Future<Result<OAuth2AccessToken>> {
+        return Future<Result<OAuth2AccessToken>> { aCompletion in
+            aCompletion?(self.token)
+        }
+    }
+
+}
